5fd21e11cb740aa09f862f94e9519bd66d16a2c6
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2023-03-25 22:24:29 +0700

    feat: add script read excel file

---
+++ scripts/word-handle.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const fs = require("fs");
+const {v4: uuidv4} = require("uuid");
+var XLSX = require("xlsx");
+
+var {utils} = XLSX;
+const {sheet_to_json} = utils;
+
+
+function capitalize(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+function getDataOfExcel() {
+  const wb = XLSX.readFile("./data/WordStudy.xlsx");
+  /* Get first worksheet */
+  const wsname = wb.SheetNames[0];
+  const ws = wb.Sheets[wsname];
+  
+  /* Convert array of arrays */
+  const listOfRow = sheet_to_json(ws, {header: 1, blankrows: false});
+  
+  const words = []
+  listOfRow.forEach((listOfColumnByRow, rowInd) => {
+    if(rowInd !== 0) {
+      const word = {}
+      listOfColumnByRow.forEach((cellData, cellInd) => {
+        if (cellInd === 0) word.id = cellData
+        if (cellInd === 1) word.unitId = cellData
+        if (cellInd === 2) word.word = capitalize(cellData);
+        if (cellInd === 3) word.answers = cellData.split(",").map(s => capitalize(s)) || []
+        if (cellInd === 4) word.sentences = cellData.split(",").map(s => capitalize(s)) || []
+        if (cellInd === 5) word.phrases = cellData.split(",").map(s => capitalize(s)) || []
+      })
+      words.push(word);
+    }
+  })
+  return words
+}
+
+const saveDataToFile = (fileName, data) => {
+  //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), "data");
+  console.info("SaveDataToFile: filename: %s, size: %s", fileName, data.length);
+  return fs.writeFileSync(jsonDirectory + fileName, JSON.stringify(data, null, 4));
+}
+
+function update() {
+  const vocabularies = require("../data/vocabularies.vi.json");
+  listOfWord.forEach(w => {
+    const wUpdateIndex = vocabularies.findIndex(x => x.id === w.id);
+    if (wUpdateIndex >= 0) {
+      vocabularies[wUpdateIndex] = {...w};
+    } else {
+      vocabularies.push(w);
+    }
+  })
+  saveDataToFile("/vocabularies.vi.json", vocabularies)
+}
+
+function reformatting(words) {
+  const wb = XLSX.readFile("./data/WordStudy.xlsx");
+  const rows = []
+  words.forEach(w => {
+    rows.push({
+      ...w,
+      id: uuidv4(),
+      answers: w.answers ? w.answers.join(",") : "",
+      sentences: w.sentences ? w.sentences.join(",") : "",
+      phrases: w.phrases ? w.phrases.join(",") : "",
+    })
+  })
+  const ws = utils.json_to_sheet(rows)
+  
+  utils.book_append_sheet(wb, ws, "Update_" + uuidv4().substring(0, 4))
+  
+  // Writing to our file
+  XLSX.writeFile(wb, './data/WordStudy.xlsx')
+}
+
+const listOfWord = getDataOfExcel()
+reformatting(listOfWord)
+console.log(listOfWord)
+update(listOfWord)
