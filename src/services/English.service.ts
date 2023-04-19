40ce53aa62ccd5d78c8e4c93c63dd68e90044ee5
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2023-04-19 09:09:36 +0700

    feat: update english service with dropdown list unit

--- src/services/English.service.ts
+++ src/services/English.service.ts
@@ -165,14 +165,104 @@ export const getListOfUnit = (): Unit[] => {
       value: 30,
     },
     {
-      label: "31. ",
+      label: "31. Shopping",
       description: "",
       value: 31,
     },
     {
-      label: "32. ",
+      label: "32. Clothing",
       description: "",
       value: 32,
+    },
+    {
+      label: "33. Holiday & Culture",
+      description: "",
+      value: 33,
+    },
+    {
+      label: "34. Holiday & Culture",
+      description: "",
+      value: 34,
+    },
+    {
+      label: "35. Romantic Relationship",
+      description: "",
+      value: 35,
+    },
+    {
+      label: "36. Traffic",
+      description: "",
+      value: 36,
+    },
+    {
+      label: "37. City & Neighborhood",
+      description: "",
+      value: 37,
+    },
+    {
+      label: "38. Describing Houses",
+      description: "",
+      value: 38,
+    },
+    {
+      label: "39. The Countryside",
+      description: "",
+      value: 39,
+    },
+    {
+      label: "40. Animals & Nature",
+      description: "",
+      value: 40,
+    },
+    {
+      label: "41. Natural Environment",
+      description: "",
+      value: 41,
+    },
+    {
+      label: "42. Music & Arts",
+      description: "",
+      value: 42,
+    },
+    {
+      label: "43. Analyzing & Evaluating",
+      description: "",
+      value: 43,
+    },
+    {
+      label: "44. Finance & Economics",
+      description: "",
+      value: 44,
+    },
+    {
+      label: "45. Giving Opinions",
+      description: "",
+      value: 45,
+    },
+    {
+      label: "46. Language Learning",
+      description: "",
+      value: 46,
+    },
+    {
+      label: "47. Technology",
+      description: "",
+      value: 47,
+    },
+    {
+      label: "48. Technology",
+      description: "",
+      value: 48,
+    },
+    {
+      label: "49. News",
+      description: "",
+      value: 49,
+    },
+    {
+      label: "50. News",
+      description: "",
+      value: 50,
     }
   ]
 }
